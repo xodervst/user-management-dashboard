321ae9c2183a759ba9b41695d4a58e9391c2a569
Vaibhav Singh Tomar <96247771+xodervst@users.noreply.github.com>
2023-11-18 01:03:27 +0530

    Create Home.js

---
+++ src/Components/Home.js
@@ -0,0 +1,19 @@
+// Home.js
+
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="bg-gray-100 min-h-screen flex items-center justify-center">
+      <div className="bg-white p-8 rounded shadow-md">
+        <h1 className="text-3xl font-bold mb-4">Welcome to My React App</h1>
+        <p className="text-gray-700 mb-6">
+          This is the home page of User Management Dashboard.
+        </p>
+        
+      </div>
+    </div>
+  );
+};
+
+export default Home;
