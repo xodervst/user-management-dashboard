f16dc38de7e95fa2126ed973ddc7505391d98bbe
Vaibhav Singh Tomar <96247771+xodervst@users.noreply.github.com>
2023-11-18 00:59:45 +0530

    Dashboard update in app.js

--- src/App.js
+++ src/App.js
@@ -2,38 +2,61 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import UserDetails from './Components/UserDetails';
 import AccountCreation from './Components/AccountCreation';
+import Home from './Components/Home';
 
 function App() {
   return (
     <Router>
-      <div className="container mx-auto p-4">
-        
-
-        <nav className="bg-green-500 p-4 mb-4">
-          <div className="container mx-auto">
-            <div className="flex items-center justify-between">
-            <ul className="flex space-x-4">
-            <li>
-              <Link to="/user-details" className="text-white font-bold text-xl hover:text-blue-700">
+      <div className="h-screen flex bg-gray-100">
+        {/* Sidebar/Vertical Navigation */}
+        <div className="bg-gray-800 text-white w-1/6 p-4 flex-shrink-0 pt-10">
+          <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
+          <ul>
+            <li className="flex items-center mb-2">
+              <Link to="/" className="block p-2 hover:bg-gray-600">
+                <span className="mr-2" role="img" aria-label="Home">
+                  🏠
+                </span>
+                Home
+              </Link>
+            </li>
+            <li className="flex items-center mb-2">
+              <Link to="/user-details" className="block p-2 hover:bg-gray-600">
+                <span className="mr-2" role="img" aria-label="User Details">
+                  👤
+                </span>
                 User Details
               </Link>
             </li>
-            <li>
-              <Link to="/account-creation" className="text-white font-bold text-xl hover:text-blue-700">
+            <li className="flex items-center mb-2">
+              <Link to="/account-creation" className="block p-2 hover:bg-gray-600">
+                <span className="mr-2" role="img" aria-label="Account Creation">
+                  🛠️
+                </span>
                 Account Creation
               </Link>
             </li>
           </ul>
+        </div>
+
+        {/* Main Content Area */}
+        <div className="flex-1 p-4">
+          {/* Top Bar */}
+          <div className="bg-gray-700 p-4 mb-4">
+            <div className="container mx-auto">
+              <div className="flex items-center justify-between">
+                <h1 className="text-white text-2xl font-bold">User Management Dashboard</h1>
+              </div>
             </div>
           </div>
-        </nav>
 
-        
-
-        <Routes>
-          <Route path="/user-details" element={<UserDetails />} />
-          <Route path="/account-creation" element={<AccountCreation />} />
-        </Routes>
+          {/* Content based on selected route */}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/user-details" element={<UserDetails />} />
+            <Route path="/account-creation" element={<AccountCreation />} />
+          </Routes>
+        </div>
       </div>
     </Router>
   );
